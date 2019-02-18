78be7759a6bcaf2f48929e14af4a787a92bbf501
Travis CI User <travis@example.org>
2019-02-18 14:18:38 +0000

    Merge pull request #15 from martinpovolny/aws_source_type
    Adding of AWS provider type.

--- js/App.js
+++ js/App.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "aa8c1ec27519f9ba7f99";
+/******/ 	var hotCurrentHash = "e89d8f165f21d13fa327";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
